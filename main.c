2a1ba88bd6ad7932d395d6a811850c927ee25f3f
Marcos Magalhães <53220617+MarcosMaga@users.noreply.github.com>
2023-05-03 18:08:28 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "TreeBin.h"
+#include "TreeBinAvl.h"
 
 int main()
 {
     srand(time(NULL));
     int num;
 
-    Node *ab = TreeInitit();
-    ab = InsertNode(ab, 12);
-    ab = InsertNode(ab, 4);
-    ab = InsertNode(ab, 2);
+    Node *ab = TreeInititAvl();
+    ab = InsertNodeAvl(ab, 12);
+    ab = InsertNodeAvl(ab, 4);
+    ab = InsertNodeAvl(ab, 2);
     //ab = InsertNode(ab, 8);
     //ab = InsertNode(ab, 6);
     //ab = InsertNode(ab, 16);
     //ab = InsertNode(ab, 19);
     //MaxDir(ab);
-    PreOrder(ab);
+    PreOrderAvl(ab);
     //printf("\n\n");
     //PosOrder(ab);
     //Path(ab, 12, 6);
